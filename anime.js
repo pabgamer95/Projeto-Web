bd38a31934964d71a0a1cf628352c8918d7853cb
RunnMaatrox <joaofranco741@gmail.com>
2024-04-30 12:08:16 +0100

    Update anime.js

--- anime.js
+++ anime.js
@@ -1,28 +1,16 @@
-function testAniListIntegration() {
-    // URL da API AniList para buscar animes populares
-    const ANILIST_API_KEY = "3paUSKjXlnAX7lK5jPtRCxN4c2JF1NigBdkGSH2O";
-    const apiUrl = "https://api.anilist.co/anime/popular";
+const animeId = 1; // ID do anime que você deseja obter informações
 
-    // Fazendo uma requisição GET para a API AniList
-    fetch(apiUrl)
-        .then(response => {
-            // Verifica se a resposta foi bem-sucedida (código de status 200)
-            if (!response.ok) {
-                throw new Error(`Erro na requisição: ${response.status}`);
-            }
-            // Retorna os dados da resposta em formato JSON
-            return response.json();
-        })
-        .then(animeData => {
-            // Aqui você pode manipular os dados dos animes populares, se necessário
-            console.log(animeData);
-            console.log("Integração da API AniList bem-sucedida!");
-        })
-        .catch(error => {
-            console.error(`Erro na requisição: ${error.message}`);
-            console.error("Falha na integração da API AniList.");
-        });
-}
-
-// Chamada da função para testar a integração da API AniList
-testAniListIntegration();
+fetch(`https://api.jikan.moe/v4/anime/${animeId}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(animeData => {
+    // Aqui você pode manipular os dados do anime conforme necessário
+    console.log(animeData);
+  })
+  .catch(error => {
+    console.error(`Erro na requisição: ${error.message}`);
+  });
