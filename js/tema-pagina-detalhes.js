27f4c98000264338697f262490818eddf8dcac5f
pabga <pedrobispocat@gmail.com>
2024-05-14 16:40:48 +0100

    pointer do Home, localStorage do dark ou light mode do site

--- js/tema-pagina-detalhes.js
+++ js/tema-pagina-detalhes.js
@@ -1,6 +1,18 @@
 let body = document.querySelector('body')
 let banner = document.getElementById('banner')
 
+// Get the theme from localStorage or set it to 'light' if not found
+let theme = localStorage.getItem('theme') || 'light';
+
+// Set the initial theme
+if (theme === 'dark') {
+    body.classList.add('dark');
+    banner.src = "../imagens/Prancheta 1.4.svg"
+    banner.classList.add('dark_banner')
+} else {
+    banner.src = "../imagens/Prancheta 2.4.svg"
+    banner.classList.remove('dark_banner') 
+}
 
 
 document.querySelector('.ball').addEventListener('click', (e)=>{
@@ -9,11 +21,13 @@ document.querySelector('.ball').addEventListener('click', (e)=>{
     body.classList.toggle('dark');
     if (document.body.classList.contains('dark')) {
         // code to execute if body has class 'dark'
+        localStorage.setItem('theme', 'dark');
         banner.src = "../imagens/Prancheta 1.4.svg"
         banner.classList.add('dark_banner')
 
       } else {
         // code to execute if body does not have class 'dark'
+        localStorage.setItem('theme', 'light');
         banner.src = "../imagens/Prancheta 2.4.svg"
         banner.classList.remove('dark_banner') 
 
--- js/tema-pagina.js
+++ js/tema-pagina.js
@@ -3,17 +3,32 @@ let banner = document.getElementById('banner')
 let news = document.getElementById('news')
 
 
+let theme = localStorage.getItem('theme') || 'light';
+if (theme === 'dark') {
+  body.classList.add('dark');
+  banner.src = "imagens/Prancheta 1.4.svg"
+  banner.classList.add('dark_banner')
+  news.classList.add('dark_news')
+
+} else {
+  banner.src = "imagens/Prancheta 2.4.svg"
+  banner.classList.remove('dark_banner') 
+  news.classList.remove('dark_news')
+}
+
 document.querySelector('.ball').addEventListener('click', (e)=>{
     e.target.classList.toggle('ball-move');
     e.target.classList.toggle('white')
     body.classList.toggle('dark');
     if (document.body.classList.contains('dark')) {
         // code to execute if body has class 'dark'
+        localStorage.setItem('theme', 'black');
         banner.src = "imagens/Prancheta 1.4.svg"
         banner.classList.add('dark_banner')
         news.classList.add('dark_news')
       } else {
         // code to execute if body does not have class 'dark'
+        localStorage.setItem('theme', 'light');
         banner.src = "imagens/Prancheta 2.4.svg"
         banner.classList.remove('dark_banner') 
         news.classList.remove('dark_news')
