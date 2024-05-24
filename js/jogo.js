daa898bc9258e363e61cbbba161b96cc35e85b1e
pabga <pedrobispocat@gmail.com>
2024-05-24 12:24:08 +0100

    joguinho

--- js/jogo.js
+++ js/jogo.js
@@ -1,7 +1,7 @@
 //board
-let tileSize = 32;
-let rows = 16;
-let columns = 16;
+const tileSize = 64;
+const rows = 16;
+const columns = 16;
 
 let board;
 let boardWidth = tileSize * columns; // 32 * 16
